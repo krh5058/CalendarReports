32332b67b2ae7187f8f461d1989ca6bca07cbfde
krh5058 <ken.r.hwang@gmail.com>
2014-05-30 14:26:06 -0400

    140530 Loaded cache
    Created get_data in DataReport, loads most recent cache or initiates CalendarReports.main() to create cache data

---
+++ DataReport.py
@@ -0,0 +1,61 @@
+#-------------------------------------------------------------------------------
+# Name:        DataReport
+# Purpose:
+#
+# Author:      krh5058
+#
+# Created:     30/05/2014
+# Copyright:   (c) krh5058 2014
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import CalendarReports
+
+##import argparse
+import os
+import sys
+from datetime import timedelta, datetime
+from utils.event import EventClass
+from utils.resources import Configure, DataStore, History
+from utils import analysis
+import re
+import pickle
+
+# Developer parameters
+debug = True
+
+### Parser for command-line arguments.
+##parser = argparse.ArgumentParser(
+##    description=__doc__,
+##    formatter_class=argparse.RawDescriptionHelpFormatter,
+##    parents=[tools.argparser])
+
+# File path
+filename = os.path.dirname(__file__)
+
+def get_data():
+    # List 'app-cache'
+    ##configure = Configure(debug,path=filename)
+    cachepath = Configure._Configure__joinpath(filename, 'app-cache')
+    listing = sorted(os.listdir(cachepath))
+
+    # TODO, What to do with other cache
+    if listing:
+        latest = max(listing) ## Latest date
+        if debug:
+            print('Using latest cached data, {0}'.format(latest))
+        return pickle.load(open(Configure._Configure__joinpath(cachepath, latest + os.sep + 'data.p'),'rb'))
+    else: ## If no cache, recursion
+        if debug:
+            print('No cache, running CalendarReports.')
+        CalendarReports.main(None)
+        main()
+
+def main():
+
+    current = get_data()
+    print('done')
+##    configure.load_cache()
+
+if __name__ == '__main__':
+    main()
